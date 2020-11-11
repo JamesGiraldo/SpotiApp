b68d0913b7a97be43f6c16399e1e9c2c3d12a559
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-11-10 21:29:05 -0500

    YAA

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,6 +10,7 @@ export class SpotifyService {
   constructor( private http: HttpClient ) { }
 
   getQuery( query: string){
+    
     const url = `https://api.spotify.com/v1/${ query }`;
     const headers = new HttpHeaders({
         'Authorization': 'Bearer BQBaWigeBWRbeN4JHSnMmvts3ZmeVf4JuBkaiFCc5DhEKv_7F6WF5ODQFRotVLfQgW3uKzaMTz6GQiSxEkCzFzysbIwxb6TzxV1sabC0-3Z3DlKIiv77T02xK4vyTZhQ_CzA1i2n23mSVDk0Eu6N5kDnZ4_PXPk'
